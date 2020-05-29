a87ed8380fcc8722e9f9514b055a452f8482a73a
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-29 15:10:14 +0100

    Create customer, film and ticket classes

---
+++ models/customer.rb
@@ -0,0 +1,14 @@
+
+
+class Customer 
+
+    attr_reader :id
+    attr_accessor :name, :funds
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @name = options['name']
+        @funds = options['funds'].to_i
+    end
+
+end
